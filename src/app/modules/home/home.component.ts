1881d35bd4cb6ffcc9298b76add97613dc9efca6
Guillermo Aiquel <gaiquel@spredfast.com>
2016-12-04 17:59:42 -0300

    home v1

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -9,6 +9,14 @@ export class HomeComponent implements OnInit {
 
   searchForm = { city: '' };
 
+  portfolio = [
+    { name: 'Paris', description: 'Yes, we built Paris', image: 'http://www.w3schools.com/bootstrap/paris.jpg'},
+    { name: 'New York', description: 'We built New York', image: 'http://www.w3schools.com/bootstrap/newyork.jpg'},
+    { name: 'San Francisco', description: 'Yes, San Fran is ours', image: 'http://www.w3schools.com/bootstrap/sanfran.jpg'}
+  ];
+
+  isCollapsed = true;
+
   constructor() { }
 
   ngOnInit() {
