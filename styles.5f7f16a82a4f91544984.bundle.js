21a74acce1288ad19c567e394e207673c386cca8
Guillermo Aiquel <gaiquel@spredfast.com>
2016-12-15 10:40:54 -0300

    property page updated

--- styles.5f7f16a82a4f91544984.bundle.js
+++ styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{461:function(n,o){},462:function(n,o){},463:function(n,o){},781:function(n,o,c){c(461),c(462),n.exports=c(463)}},[781]);
+webpackJsonp([1,2],{461:function(n,o){},462:function(n,o){},463:function(n,o){},784:function(n,o,c){c(461),c(462),n.exports=c(463)}},[784]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
