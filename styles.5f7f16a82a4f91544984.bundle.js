583b1b6fbda91cf0284e6fda00dc4eb7d7ed1b52
Guillermo Aiquel <gaiquel@spredfast.com>
2016-12-19 00:45:50 -0300

    new version

--- styles.5f7f16a82a4f91544984.bundle.js
+++ styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{463:function(n,o){},464:function(n,o){},465:function(n,o){},804:function(n,o,c){c(463),c(464),n.exports=c(465)}},[804]);
+webpackJsonp([1,2],{463:function(n,o){},464:function(n,o){},465:function(n,o){},812:function(n,o,c){c(463),c(464),n.exports=c(465)}},[812]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
